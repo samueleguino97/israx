a29c453e2505c170f8dff0bf19fce97784dc0dfd
Sam Eguino <samueleguino97@gmail.com>
2025-04-27 15:05:10 -0400

    fix build

--- src/app/normativo/page.tsx
+++ src/app/normativo/page.tsx
@@ -148,7 +148,6 @@ export default function Normativo() {
               denegando el cambio de nombre. Ley N° 025 - Ley del Órgano
               Judicial (Tribunal Supremo de Justicia, 2010)
               <p className="relative z-10 mt-2 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <LinkIcon className="h-6 w-6 flex-none" />
                 <Link href="https://tsj.bo/wp-content/uploads/2019/11/ley-025-ley-del-organo-judicial.pdf">
                   https://tsj.bo/wp-content/uploads/2019/11/ley-025-ley-del-organo-judicial.pdf
                 </Link>
